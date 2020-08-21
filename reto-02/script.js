0773be43613cf067677fb122e082e8d6ae3177d2
Pablo Bustamante <pablo@4topixel.com>
2020-08-20 20:31:06 -0500

    ✨ Reto 02
    Crear un algoritmo con un condicional

---
+++ reto-02/script.js
@@ -0,0 +1,21 @@
+var numero01 = parseInt(prompt('Escribe un número'));
+var mensaje = prompt('Escribe la palabra mágica');
+var numero02 = prompt('Escribe un número del 1 al 100');
+
+if (numero01 > 1000) {
+    console.info('¡Tu número es mayor a 1000!');
+} else {
+    console.info('¡Tu número es menor a 1000!');
+};
+
+if (mensaje === 'hola') {
+    console.info('Conoces la palabra mágica');
+} else {
+    console.error('No conoces la palabra mágica');
+};
+
+if (numero02 > 10 && numero02 < 50) {
+    console.info('Tu número está entre 10 y 50');
+} else {
+    console.error('Tu número **NO** está entre 10 y 50');
+};
